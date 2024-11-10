090e16b7505cecbac7ff0640127f82795aa335dc
kanjareepiya <161728806+kanjareepiya@users.noreply.github.com>
2024-11-10 15:16:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,14 @@ culmen_depth = st.number_input('Culmen Depth (mm)', min_value=10.0, max_value=10
 flipper_length = st.number_input('Flipper Length (mm)', min_value=100.0, max_value=250.0, value=181.0)
 body_mass = st.number_input('Body Mass (g)', min_value=2000.0, max_value=6000.0, value=3750.0)  # เปลี่ยน min_value และ max_value เป็น float
 
+input_features = np.array([culmen_length,culmen_depth,flipper_length,body_mass])
 # Create a DataFrame with the input data
-input_data = pd.DataFrame({
-    'culmen_length_mm': [culmen_length],
-    'culmen_depth_mm': [culmen_depth],
-    'flipper_length_mm': [flipper_length],
-    'body_mass_g': [body_mass]
-})
+#input_data = pd.DataFrame({
+ #   'culmen_length_mm': [culmen_length],
+  #  'culmen_depth_mm': [culmen_depth],
+   # 'flipper_length_mm': [flipper_length],
+    #'body_mass_g': [body_mass]
+#})
 
 # Standardize the input features using the same scaler as during training
 
