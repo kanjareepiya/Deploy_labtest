d5afa12d212d6b950dc319c922e4bb3b6fec9093
kanjareepiya <161728806+kanjareepiya@users.noreply.github.com>
2024-11-10 15:18:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
